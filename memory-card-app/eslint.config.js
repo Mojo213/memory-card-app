7d0efdd90275c9930a4803fe120eb366c5835e08
Mojo213 <104395212+Mojo213@users.noreply.github.com>
2025-04-18 09:32:33 +0000

    update eslint config add prettier config

--- memory-card-app/eslint.config.js
+++ memory-card-app/eslint.config.js
@@ -1,11 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: {
+      js,
+      prettier: pluginPrettier,
+    },
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      ...pluginPrettier.configs.recommended.rules,
+    },
+  },
   pluginReact.configs.flat.recommended,
+  configPrettier, 
 ]);
