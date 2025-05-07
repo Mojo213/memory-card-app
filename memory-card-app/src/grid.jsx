1764d4b1528fc92d8f1cdd08788ce17dbfdf7be7
Mojo213 <104395212+Mojo213@users.noreply.github.com>
2025-05-07 15:24:01 +0000

    update img element size

--- memory-card-app/src/grid.jsx
+++ memory-card-app/src/grid.jsx
@@ -26,7 +26,7 @@ export default function GridComponent() {
       {cards.map(character=> (
           <div key={character.id}>
            <h1>{character.name}</h1>
-           <img src={character.image} alt={character.name} style={{ width: '150px', height: '250px' }} />
+           <img src={character.image} alt={character.name} style={{ width: '150px', height: '260px' }} />
           </div> 
       ))}
     </div>
